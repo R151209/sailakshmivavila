dc2fc02f30a4c65fea38812ec7518c540d0b6104
R151209 <47173973+R151209@users.noreply.github.com>
2019-02-04 08:56:21 +0530

    Add files via upload

---
+++ sam_phase.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+f1=input("enter f1 value:")
+f2=input("enter f2 value:")
+fs=input("enter fs value:")
+n=input("enter no.of samples:")
+p1=input("enter phase for sin:")
+p2=input("enter phase for cos:")
+t=np.arange(n)
+x1=np.sin(2*np.pi*(f1/fs)+p1*t)
+plt.subplot(3,1,1)
+plt.plot(t,x1)
+plt.title('sin sampling')
+plt.xlabel('samples')
+plt.ylabel('amp')
+x2=np.cos(2*np.pi*(f2/fs)+p2*t)
+plt.subplot(3,1,2)
+plt.plot(t,x2)
+plt.title('cos sampling')
+x=x1+x2
+plt.subplot(3,1,3)
+plt.plot(t,x)
+plt.title('sum of two waves')
+plt.show( )
+
